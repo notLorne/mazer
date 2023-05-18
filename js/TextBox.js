b324caab6d9b639f7f9e7186ed7a63e9a82aa5f6
notLorne <113377659+notLorne@users.noreply.github.com>
2023-05-18 12:56:43 -0400

    textbee

--- js/TextBox.js
+++ js/TextBox.js
@@ -11,29 +11,43 @@ class TextBox {
     isTypeWriter;
     typeCounter;
     typeTrigger;
+    letterColorCode;
+    textBoxColorCode;
 
     xPos;
     yPos;
     xAnchor;
     yAnchor;
     destinationContext;
+    boxSize;
     textSize;
 
-    // letterColor, bgColor, typeEffect, typeTrigger, maxLettersWidth, maxLines, // TO ADD TO CONSTRUCTOR
-    constructor(text, context, posX, posY){
-
-        this.letterWidth = 5;
-        this.letterHeight = 7;
-        this.textSize = 2;
+    // letterColor, bgColor, typeEffect, boxSize // TO ADD TO CONSTRUCTOR
+    //type shall include textSize, number of line max and line max length.
+    //As of now, only 81 chars can be entered on one line. max could be two line, for 162 character max for a message.
+    //we might also want to have smaller boxes. This process doesnt really work optimally like that.
+    constructor(text, context, posX, posY, sizeOfBox){
+        
+        this.letterWidth = 5; //CONST - width of the binary array.
+        this.letterHeight = 7; //CONST - height of the binary array.
+        //Context and textbox position.
         this.destinationContext = context;
         this.xPos = posX;
         this.yPos = posY;
+        //Box size adjustment.
+        this.boxSize = ( sizeOfBox == undefined ) ? 1 : sizeOfBox;
+
+        this.textSize = 2 * this.boxSize;
 
         this.textLength = text.length;
+        this.textLines = Math.ceil(this.textLength / 81);
+
+        console.log(this.textLines);
 
         this.outputText = text;
-        this.charArray = [];
 
+        // Binary codes for each letter of the string
+        this.charArray = [];
         for ( let k = 0; k < this.outputText.length; ++k ) {
             for (let j = 0; j < charTable.length; ++j ) {
                 if (Object.keys(charTable[j]) == this.outputText.charAt(k)) {
@@ -42,30 +56,49 @@ class TextBox {
                 }
             } 
         }
-        console.log(this.charArray);
+        //Should be changed because that wont work with animations...
         this.drawLetters(this.charArray)
     }
 
     drawLetters(letterArrays) {
-
+        
+        var letterAdj;
+        //Set context and smoothing
         const textContext = document.getElementById(this.destinationContext).getContext("2d");
-        textContext.fillStyle = "rgb(255,255,255)";
-
-        textContext.fillRect(this.xPos - 4, 
-                             this.yPos - 4, 
-                             (this.letterWidth + 1) * this.textSize * this.textLength + 8, 
-                             (this.letterHeight + 2 + 4) * this.textSize)
-
+        textContext.imageSmoothingEnabled= false;
+        //Draw textbox background
+        textContext.fillStyle = "rgb(0,255,255)";
+        textContext.fillRect(this.xPos - 8, 
+                             this.yPos - 8, 
+                             (this.letterWidth + 1) * this.textSize * this.textLength + 16, 
+                             (this.letterHeight + 6) * this.textSize + 4)
+        //Draw textbox outline
+        textContext.strokeStyle = "rgb(255,0,0)";
+        textContext.strokeRect(this.xPos - 6, 
+                            this.yPos - 6, 
+                            (this.letterWidth + 1) * this.textSize * this.textLength + 12, 
+                            (this.letterHeight + 6) * this.textSize);
+        
+        
+        //Draw characters
         textContext.fillStyle = "rgb(255,0,0)";
         for(let c = 0; c < letterArrays.length; ++c ){
-            var testString = letterArrays[c][0];
 
+            var currentChar = letterArrays[c][0];
+            //Adjust character height for small case g, j, p, q, y and comma ",".
+            letterAdj = (   currentChar == "01110100011000101111000011000101110" || 
+                            currentChar == "00001000000001100001000011000101110" ||
+                            currentChar == "11110100011000110001111101000010000" ||
+                            currentChar == "01111100011000110001011110000100001" ||
+                            currentChar == "00000000000000000000001100001000100" ||
+                            currentChar == "10001100011000110001011110000101110" ) ? 3 : 0;
+            //Draw letter pixels
             for ( let k = 0; k < 7; ++k ) {
                 for ( let j = 0; j < 5; ++j ) {
 
-                    if(testString.charAt((k * 5) + j) == 1) {
+                    if(currentChar.charAt((k * 5) + j) == 1) {
                         textContext.fillRect(this.xPos + (j * this.textSize) + (c * (this.letterWidth + 1) * this.textSize), 
-                                             this.yPos + (k * this.textSize), 
+                                             this.yPos + (k * this.textSize) + letterAdj, 
                                              this.textSize, 
                                              this.textSize);
                     }
@@ -73,6 +106,8 @@ class TextBox {
             }
         }
     }
+
+
 }
 
-var boxte = new TextBox("Hi I hope you are having a nice day", "canvas", 200, 133);
+var boxte = new TextBox("Thqs ps the f.st 50ne.Thqs ps tddhe fyrst line.", "canvas", 12, 12);
